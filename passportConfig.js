f52227ee64e1b3f0d400abdbe6cc58b0474e9a42
mary <laguta113@gmail.com>
2024-08-24 09:43:28 +0500

    Add Google Authentication

--- passportConfig.js
+++ passportConfig.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
+require('dotenv').config();
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const pool = require('./db');
 const bcrypt = require('bcrypt');
 
@@ -28,6 +30,31 @@ passport.use(
         }
     )
 );
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            const userResult = await pool.query('SELECT * FROM users WHERE oauth_provider = $1 AND oauth_id = $2', ['google', profile.id]);
+
+            if (userResult.rows.length > 0) {
+                return done(null, userResult.rows[0]);
+            } else {
+                const newUserResult = await pool.query(`
+                    INSERT INTO users (username, email, oauth_provider, oauth_id, created_at, updated_at)
+                    VALUES ($1, $2, $3, $4, NOW(), NOW())
+                    RETURNING *`, [profile.displayName, profile.emails[0].value, 'google', profile.id]);
+
+                return done(null, newUserResult.rows[0]);
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
