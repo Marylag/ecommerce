7bcd72261ab4c5eb9110a95b3dfb29d1ff843474
mary <laguta113@gmail.com>
2024-08-24 09:45:39 +0500

    Add more functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const session = require('express-session');
 const passport = require('./passportConfig');
 const authRoutes = require('./routes/auth');
@@ -9,13 +9,16 @@ const orderRoutes = require('./routes/orders');
 const swaggerSpec = require('./config/swagger');
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
+const cors = require('cors');
 const fs = require('fs');
 require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(
   session({
@@ -28,19 +31,21 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Swagger setup for API documentation
 const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yml', 'utf8'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Default home route
 app.get('/', (req, res) => {
   res.send('Welcome to the E-commerce API');
 });
 
+// API Routes
 app.use('/orders', passport.authenticate('local', { session:false }), orderRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/cart', cartRoutes);
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
