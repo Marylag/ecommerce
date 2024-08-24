3da4f735fbe899ae59eba5d67f5caa16c685e367
mary <laguta113@gmail.com>
2024-08-24 09:40:51 +0500

    Fix fetching product issue

--- utils/productService.js
+++ utils/productService.js
@@ -3,18 +3,15 @@ const pool = require('../db');
 const getProductPrice = async (productId) => {
     try {
         const result = await pool.query('SELECT price FROM products WHERE id = $1', [productId]);
-
         if (result.rows.length === 0) {
             throw new Error('Product not found');
         }
-
         return result.rows[0].price;
-    } catch (err) {
-        console.error('Error fetching product proce:', err);
-        throw err;
+    } catch (error) {
+        throw error;
     }
 };
 
 module.exports = {
-    getProductPrice,
+    getProductPrice
 };
