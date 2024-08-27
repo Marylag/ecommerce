b8aa6515b1e6670bc3d984937d6b0a645e22d977
mary <laguta113@gmail.com>
2024-08-27 14:17:09 +0500

    Added Logic for Google Auth redirection

--- view/src/pages/Home.js
+++ view/src/pages/Home.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const token = queryParams.get('token');
+
+        if (token) {
+            localStorage.setItem('token', token);
+            navigate('/protected')
+        }
+    }, [navigate]);
+
     return (
         <div>
             <h1>Home Page</h1>
