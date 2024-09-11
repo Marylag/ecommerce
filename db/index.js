cf5be849e1f04a54e587c0fd987e4f498e369d48
mary <laguta113@gmail.com>
2024-09-11 20:27:25 +0500

    Fixed error

--- db/index.js
+++ db/index.js
@@ -6,8 +6,6 @@ require('dotenv').config({
     path: path.join(__dirname, 'dev.env')
 });
 
-const { Pool } = require('pg');
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
